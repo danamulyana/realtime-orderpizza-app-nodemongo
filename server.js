d495309f11a94eb4da3e8d65edf42abdd2bec24e
Dana Mulyana <danabontot@gmail.com>
2022-07-30 22:00:27 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path')
+const expressLayout = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'))
+app.use(expressLayout);
+app.set('views', path.join(__dirname,'/resources/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    res.render('home', {layout: false})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running http://localhost:${PORT}`);
+})
